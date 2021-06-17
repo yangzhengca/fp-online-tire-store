e5bfb4f7650d6b8c35bbc82aeb00556f50c89d44
yangzhengca <yangzhengca@gmail.com>
2021-06-17 12:37:38 -0600

    Added products

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -4,7 +4,7 @@ var mongoose=require('mongoose');
 
 
 // const DATABASE_URL="mongodb://localhost:27017/shopping"
-const DATABASE_URL="mongodb+srv://dbneo:neo3120@cluster0.m1pot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const DATABASE_URL="mongodb+srv://dbneo:@cluster0.m1pot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 mongoose.connect(DATABASE_URL,{useNewUrlParser:true,useUnifiedTopology: true});
 const db=mongoose.connection;
@@ -13,53 +13,213 @@ db.once('open',()=>console.log('Connected to Database ...'));
 
 
 var products=[
+    // AT tires
     new Product({
-        imagePath:"https://i.imgur.com/pQMplfB.jpg",
-        title:'18" AT Tire',
+        imagePath:"https://i.imgur.com/9VwXoiF.png",
+        title:'CAPSULE AT Tire',
         description:"18 inches all terrain tire",
-        width:230,
-        aspectRatio:65,
+        width:255,
+        aspectRatio:55,
         diameter:18,
-        price:200,
+        price:289,
         inStock:18
     }),
-
     new Product({
-        imagePath:"https://i.imgur.com/pQMplfB.jpg",
-        title:'20" AT Tire',
-        description:"20 inches all terrain tire",
-        price:250,
-        size:20
+        imagePath:"https://i.imgur.com/9VwXoiF.png",
+        title:'CAPSULE AT Tire',
+        description:"17 inches all terrain tire",
+        width:265,
+        aspectRatio:70,
+        diameter:17,
+        price:269,
+        inStock:20
+    }),
+    //MT tires
+    new Product({
+        imagePath:"https://i.imgur.com/pAQiCrD.png",
+        title:'CAPSULE MT Tire',
+        description:"17 inches mud terrain tire",
+        width:265,
+        aspectRatio:70,
+        diameter:17,
+        price:279,
+        inStock:16
     }),
     new Product({
-        imagePath:"https://i.imgur.com/GFO9a2w.jpg",
-        title:'17" All Season Tire',
-        description:"17 inches all season tire",
-        price:150,
-        size:17
+        imagePath:"https://i.imgur.com/pAQiCrD.png",
+        title:'CAPSULE MT Tire',
+        description:"16 inches mud terrain tire",
+        width:265,
+        aspectRatio:75,
+        diameter:16,
+        price:259,
+        inStock:26
+    }),
+    //Sport tires
+    new Product({
+        imagePath:"https://i.imgur.com/EmH0xgu.png",
+        title:'CAPSULE Sport Tire',
+        description:"16 inches sport tire",
+        width:225,
+        aspectRatio:70,
+        diameter:16,
+        price:299,
+        inStock:27
     }),
     new Product({
-        imagePath:"https://i.imgur.com/GFO9a2w.jpg",
-        title:'19" All Season Tire',
-        description:"19 inches all season tire",
-        price:200,
-        size:19
+        imagePath:"https://i.imgur.com/EmH0xgu.png",
+        title:'CAPSULE Sport Tire',
+        description:"17 inches sport tire",
+        width:225,
+        aspectRatio:60,
+        diameter:17,
+        price:319,
+        inStock:12
     }),
     new Product({
-        imagePath:"https://i.imgur.com/2Ev56dW.png",
-        title:'17" Winter Tire',
+        imagePath:"https://i.imgur.com/EmH0xgu.png",
+        title:'CAPSULE Sport Tire',
+        description:"18 inches sport tire",
+        width:225,
+        aspectRatio:55,
+        diameter:18,
+        price:339,
+        inStock:22
+    }),
+    //performence tires
+    new Product({
+        imagePath:"https://i.imgur.com/XlVWKtb.png",
+        title:'CAPSULE Performence Tire',
+        description:"18 inches performence tire",
+        width:225,
+        aspectRatio:40,
+        diameter:18,
+        price:369,
+        inStock:16
+    }),
+    new Product({
+        imagePath:"https://i.imgur.com/XlVWKtb.png",
+        title:'CAPSULE Performence Tire',
+        description:"17 inches performence tire",
+        width:225,
+        aspectRatio:45,
+        diameter:17,
+        price:349,
+        inStock:18
+    }),
+    new Product({
+        imagePath:"https://i.imgur.com/XlVWKtb.png",
+        title:'CAPSULE Performence Tire',
+        description:"17 inches performence tire",
+        width:225,
+        aspectRatio:60,
+        diameter:17,
+        price:349,
+        inStock:16
+    }),
+    //winter tires-1
+    new Product({
+        imagePath:"https://i.imgur.com/xWZIliG.png",
+        title:'CAPSULE Winter Tire',
         description:"17 inches winter tire",
-        price:200,
-        size:17
+        width:225,
+        aspectRatio:65,
+        diameter:17,
+        price:189,
+        inStock:36
     }),
     new Product({
-        imagePath:"https://i.imgur.com/2Ev56dW.png",
-        title:'19" Winter Tire',
-        description:"19 inches winter tire",
-        price:250,
-        size:19
-    })
-
+        imagePath:"https://i.imgur.com/xWZIliG.png",
+        title:'CAPSULE Winter Tire',
+        description:"16 inches winter tire",
+        width:225,
+        aspectRatio:65,
+        diameter:16,
+        price:179,
+        inStock:28
+    }),
+    //winter tires
+    new Product({
+        imagePath:"https://i.imgur.com/v07JBkU.png",
+        title:'CAPSULE Advanced Winter Tire',
+        description:"17 inches advanced winter tire",
+        width:225,
+        aspectRatio:55,
+        diameter:17,
+        price:209,
+        inStock:25
+    }),
+    new Product({
+        imagePath:"https://i.imgur.com/v07JBkU.png",
+        title:'CAPSULE Advanced Winter Tire',
+        description:"18 inches advanced winter tire",
+        width:225,
+        aspectRatio:45,
+        diameter:18,
+        price:229,
+        inStock:18
+    }),
+    //all season tires
+    new Product({
+        imagePath:"https://i.imgur.com/P6qdvvw.png",
+        title:'CAPSULE All Season Tire',
+        description:"18 inches all season tire",
+        width:225,
+        aspectRatio:55,
+        diameter:18,
+        price:209,
+        inStock:17
+    }),
+    new Product({
+        imagePath:"https://i.imgur.com/P6qdvvw.png",
+        title:'CAPSULE All Season Tire',
+        description:"17 inches all season tire",
+        width:205,
+        aspectRatio:50,
+        diameter:17,
+        price:179,
+        inStock:34
+    }),
+    new Product({
+        imagePath:"https://i.imgur.com/P6qdvvw.png",
+        title:'CAPSULE All Season Tire',
+        description:"17 inches all season tire",
+        width:205,
+        aspectRatio:50,
+        diameter:17,
+        price:179,
+        inStock:34
+    }),
+    new Product({
+        imagePath:"https://i.imgur.com/P6qdvvw.png",
+        title:'CAPSULE All Season Tire',
+        description:"16 inches all season tire",
+        width:205,
+        aspectRatio:55,
+        diameter:16,
+        price:159,
+        inStock:34
+    }),
+    new Product({
+        imagePath:"https://i.imgur.com/P6qdvvw.png",
+        title:'CAPSULE All Season Tire',
+        description:"15 inches all season tire",
+        width:195,
+        aspectRatio:55,
+        diameter:15,
+        price:149,
+        inStock:34
+    }),
+    new Product({
+        imagePath:"https://i.imgur.com/P6qdvvw.png",
+        title:'CAPSULE All Season Tire',
+        description:"14 inches all season tire",
+        width:185,
+        aspectRatio:60,
+        diameter:14,
+        price:139,
+        inStock:30
+    }),
 ];
 
 
