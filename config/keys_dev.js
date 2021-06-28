d3738f89556d15bde9c90b0255ec68f8a1fad793
Yang <82341009+yangzhengca@users.noreply.github.com>
2021-06-28 16:10:54 -0600

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,7 +0,0 @@
-module.exports ={
-    stripePublicKey:'pk_test_51J1nqnKmu8GjevWW184jdbbzj8Pr7DsBL8Q2LzWhLFXg5QuXAKrP1fIxmCuZCvHwspnlfIWyQoFT7qbmWYTtRwa000YvsF9qNF',
-    stripeSecretKey:'sk_test_51J1nqnKmu8GjevWWdez7lLUv84Rc8s3NRL8bjqMMRQWBje484fEBncnrsczucskizegNQ77zQtEeEYXIfmn5w7j000ZNNYqG5J',
-    mongoURI:'mongodb+srv://dbneo:neo3120@cluster0.m1pot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    secretOrKey:'SECRET',
-    myDomain:'http://localhost:3000'
-}
